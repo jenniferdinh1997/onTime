b0b4c3d79e090990710099b593713895c9ad6e14
James Haff <jameshaff@Jamess-MBP.attlocal.net>
2022-02-18 10:54:54 -0600

    update node modules

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,49 +1,52 @@
-import React, {useState} from 'react';
-import { Redirect, Route, Switch } from 'react-router-dom';
-import './App.css';
-import SignupPage from '../SignupPage/SignupPage';
-import LoginPage from '../LoginPage/LoginPage';
-import userService from '../../utils/userService'
-
+import React, { useState } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import SignupPage from "../SignupPage/SignupPage";
+import LoginPage from "../LoginPage/LoginPage";
+import userService from "../../utils/userService";
 
 function App() {
-
-  const [user, setUser] = useState(userService.getUser()) // getUser decodes our JWT token, into a javascript object
-  // this object corresponds to the jwt payload which is defined in the server signup or login function that looks like 
+  const [user, setUser] = useState(userService.getUser()); // getUser decodes our JWT token, into a javascript object
+  // this object corresponds to the jwt payload which is defined in the server signup or login function that looks like
   // this  const token = createJWT(user); // where user was the document we created from mongo
 
-  function handleSignUpOrLogin(){
-    setUser(userService.getUser()) // getting the user from localstorage decoding the jwt
+  function handleSignUpOrLogin() {
+    setUser(userService.getUser()); // getting the user from localstorage decoding the jwt
   }
 
-  function handleLogout(){
+  function handleLogout() {
     userService.logout();
-    setUser({user: null})
+    setUser(null);
+  }
+
+  if (user) {
+    return (
+      <Routes>
+        <Route path="/" element={<h1>This is Home Page!</h1>} />
+        <Route
+          path="/login"
+          element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+        <Route
+          path="/signup"
+          element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+        />
+      </Routes>
+    );
   }
 
   return (
-    <div className="App">
-      <Switch>
-          <Route exact path="/login">
-             <LoginPage handleSignUpOrLogin={handleSignUpOrLogin}/>
-          </Route>
-          <Route exact path="/signup">
-             <SignupPage handleSignUpOrLogin={handleSignUpOrLogin}/>
-          </Route>
-          {userService.getUser() ? 
-            <> 
-             <Switch>
-                <Route exact path="/">
-                    Home PAGE COMPONENT WOULD GO HEREE
-                </Route>
-            </Switch>
-            </>
-            :
-            <Redirect to='/login'/>
-          }
-  
-      </Switch>
-    </div>
+    <Routes>
+      <Route
+        path="/login"
+        element={<LoginPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
+      <Route
+        path="/signup"
+        element={<SignupPage handleSignUpOrLogin={handleSignUpOrLogin} />}
+      />
+      <Route path="/*" element={<Navigate to="/login" />} />
+    </Routes>
   );
 }
 
