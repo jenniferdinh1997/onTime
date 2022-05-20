9c414c88ca275563c5561bdbf40e22a63db28b81
JimHaff <Jimmyhaff3@gmail.com>
2022-05-20 15:28:27 -0500

    Update SignupPage.jsx

--- src/pages/SignupPage/SignupPage.jsx
+++ src/pages/SignupPage/SignupPage.jsx
@@ -9,9 +9,9 @@ export default function SignUpPage(props) {
     <>
       <h1>Signup PAGE</h1>
       <ul>
-        <li>Read the Login Model, You can change it to fit your needs</li>
+        <li>Read the User Model, You can change it to fit your needs</li>
         <li>
-          Make sure you read the Login Controller, to know how it is setup to
+          Make sure you read the Signup up func in the User Controller, to know how it is setup to
           find the user!
         </li>
       </ul>
