3eefdf9a9eddc1ef306775680ae4b66114c9d23a
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-05-25 15:44:26 -0700

    ride confirmation function

--- src/components/RideConfirmation/RideConfirmation.jsx
+++ src/components/RideConfirmation/RideConfirmation.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RideConfirmation.css';
+import {MDBBtn} from 'mdb-react-ui-kit';
 
 export default function RideConfirmation({ user, currentRide }) {
+    const [showConfirm, setShowConfirm] = useState(true)
+
+    function handleShowConfirm(e) {
+        e.preventDefault();
+        setShowConfirm(false)
+    }
+
     return (
+        <>
+        {showConfirm && (
         <div className='card'>
             <div class='card-body'>
                 <h5 className='card-title'>Your driver is on the way!</h5>
@@ -12,7 +22,10 @@ export default function RideConfirmation({ user, currentRide }) {
                 <p>{currentRide.date}</p>
                 <p>Pick Up: {currentRide.pickup}</p>
                 <p>Drop Off: {currentRide.dropoff}</p>
+                <MDBBtn onClick={handleShowConfirm}>Complete Ride</MDBBtn>
             </div>
         </div>
+        )}
+        </>
     )
 }
