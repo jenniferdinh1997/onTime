3873ed7e8ba53f4805450b75b0cdfffdb47c4ec4
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-06-07 23:36:46 -0700

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,70 +1,40 @@
 import React from 'react';
-import {
-    MDBNavbar,
-    MDBNavbarNav,
-    MDBNavbarItem,
-    MDBNavbarLink,
-    MDBContainer,
-    MDBNavbarBrand
-  } from 'mdb-react-ui-kit';
-import { Image, Icon } from 'semantic-ui-react';
+import './Header.css';
 
 export default function Header({ user, handleLogout, handleShowHistory }) {
     if (user) {
         return (
             <>
                 <header>
-                    <MDBNavbar expand='sm' light bgColor='white' fixed='top'>
-                        <MDBContainer fluid>                          
-                            <div className='collapse navbar-collapse'>
-                                <MDBNavbarNav left className='me-auto'>
-                                    <MDBNavbarItem>
-                                        <MDBNavbarBrand href='/'><Icon name='car' /></MDBNavbarBrand>
-                                    </MDBNavbarItem>
-                                    <MDBNavbarItem>
-                                        <MDBNavbarLink href='/#about'>About Us</MDBNavbarLink>
-                                    </MDBNavbarItem>
-                                    <MDBNavbarItem>
-                                        <MDBNavbarLink href='/trip'>Request a Ride</MDBNavbarLink>
-                                    </MDBNavbarItem>
-                                    <MDBNavbarItem>
-                                        <MDBNavbarLink href='/trip/#history' onClick={handleShowHistory}>See Ride History</MDBNavbarLink>
-                                    </MDBNavbarItem>
-                                </MDBNavbarNav>
-                                <MDBNavbarNav className='ms-auto'>
-                                    <Image src={user.photoUrl} avatar></Image>
-                                    <MDBNavbarItem>
-                                        <MDBNavbarLink href={`/${user.name}`}>{user.name}</MDBNavbarLink>
-                                    </MDBNavbarItem>
-                                    <MDBNavbarItem>
-                                        <MDBNavbarLink href='' onClick={handleLogout}>Log Out</MDBNavbarLink>
-                                    </MDBNavbarItem>
-                                </MDBNavbarNav>
-                            </div>
-                        </MDBContainer>
-                    </MDBNavbar>
-        </header>
-    </>
+                    <nav>
+                        <div className='left-nav'>
+                            <a href='/'>icon</a>
+                            <a href='/#about'>About Us</a>
+                            <a href='/trip'>Ride</a>
+                            <a href='/trip/#history' onClick={handleShowHistory}>See History</a>
+                        </div>                    
+                        
+                        <div className='right-nav'>
+                            <img src={user.photoUrl} className='nav-avatar' />
+                            <a href={`/${user.name}`}>{user.name}</a>
+                            <a href='' onClick={handleLogout}>Log Out</a>
+                        </div>
+                    </nav>
+                </header>
+            </>
     )
 }
 
     return (
         <>
-            <MDBNavbar expand='sm' light bgColor='white' fixed='top'>
-                <MDBContainer fluid>
-                    <MDBNavbarNav className='ms-auto'>
-                        <MDBNavbarItem>
-                            <MDBNavbarBrand href='/'></MDBNavbarBrand>
-                        </MDBNavbarItem>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='/login'>Log In</MDBNavbarLink>
-                        </MDBNavbarItem>
-                        <MDBNavbarItem>
-                            <MDBNavbarLink href='/signup'>Sign Up</MDBNavbarLink>
-                        </MDBNavbarItem>
-                    </MDBNavbarNav>
-                </MDBContainer>
-            </MDBNavbar>
+            <header>
+                <nav>
+                    <div className='right'>
+                        <a href='/login'>Log In</a>
+                        <a href='/signup'>Sign Up</a>
+                    </div>
+                </nav>
+            </header>
         </>
     )
 }
