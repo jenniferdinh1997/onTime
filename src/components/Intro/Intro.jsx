fac05747d1840d13da8f7a02397ad05a0d6415e7
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-06-07 20:23:39 -0700

    fix intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import './Intro.css';
 
 export default function Intro() {
     return (
-    <>
-        <h1>onTime</h1>
-        <h2>Never miss another doctor's appointment</h2>
-    </>
+    <div className='body'>
+        <div className='content'>
+            <h1>onTime</h1>
+            <h3>Never miss another doctor's appointment</h3>
+
+            <div className='buttons'>
+                <button>
+                    <span class="transition"></span>
+                    <span class="gradient"></span>
+                    <span class="label">Learn More</span>
+                </button>
+                <button>
+                    <span class="transition"></span>
+                    <span class="gradient"></span>
+                    <span class="label">Sign Up to Drive</span>
+                </button>
+            </div>
+        </div>
+    </div>
     )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,26 +6,25 @@ import './Home.css';
 
 export default function Home({ user, handleLogout }) {
     return (
-        <>
-        <Header user={user} handleLogout={handleLogout} />
-        <Intro />
-        <br />
-        <div className='p-5 text-center bg-light'>
-            <h4 id="about">About Us</h4>
-            <p>Our founder had previously worked as a medical assistant at a clinic and noticed that many of
-            the patients would either be late or cancel their appointments due to missing the bus, not having
-            any family members to provide rides, or miscommunicating with their rideshare drivers.</p>
-            <br />
-            <p>How do we get these patients to their doctor's appointments in the most efficient and patient friendly 
-            way possible? Enter onTime. onTime is a rideshare app that matches patients specifically to a driver that
-            speaks the same language and has a wheelchair accessible vehicle if needed. Gone are the days where 
-            language barriers prevent patients from getting to their destination. Patients will no longer have to struggle
-            to fit their walking devices and wheelchairs into their cars because onTime will specifically match a patient
-            with special accommodations to a car that fits exactly what they need.
-            </p>
-            <br />
-            <p>Drivers are questioned and interviewed thoroughly to ensure quality and compassionate care to passengers.</p>
+        <div className='whole-body'>
+            <Header user={user} handleLogout={handleLogout} />
+            <Intro />
+            <div className='p-5 text-center bg-light'>
+                <h4 id="about">About Us</h4>
+                <p>Our founder had previously worked as a medical assistant at a clinic and noticed that many of
+                the patients would either be late or cancel their appointments due to missing the bus, not having
+                any family members to provide rides, or miscommunicating with their rideshare drivers.</p>
+                <br />
+                <p>How do we get these patients to their doctor's appointments in the most efficient and patient friendly 
+                way possible? Enter onTime. onTime is a rideshare app that matches patients specifically to a driver that
+                speaks the same language and has a wheelchair accessible vehicle if needed. Gone are the days where 
+                language barriers prevent patients from getting to their destination. Patients will no longer have to struggle
+                to fit their walking devices and wheelchairs into their cars because onTime will specifically match a patient
+                with special accommodations to a car that fits exactly what they need.
+                </p>
+                <br />
+                <p>Drivers are questioned and interviewed thoroughly to ensure quality and compassionate care to passengers.</p>
+            </div>
         </div>
-        </>
     )
 }
