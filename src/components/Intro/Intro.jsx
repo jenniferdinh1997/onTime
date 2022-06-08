f88f5032657ed738979bcf001c444042fe68c6a0
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-06-07 17:27:37 -0700

    add intro component

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Intro() {
+    return (
+    <>
+        <h1>onTime</h1>
+        <h2>Never miss another doctor's appointment</h2>
+    </>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Intro from '../../components/Intro/Intro';
 import { HashLink } from 'react-router-hash-link';
 import './Home.css';
 
 export default function Home({ user, handleLogout }) {
     return (
         <>
-       <Header user={user} handleLogout={handleLogout} />
-        <div className='p-5 text-center bg-light'>
-            <h1 className='mb-3'>onTime</h1>
-            <h4 className='mb-3'>Never miss another doctor's appointment</h4>
-            <HashLink className='btn btn-primary' to='/#about' role='button'>Learn More</HashLink>
-        </div>
+        <Header user={user} handleLogout={handleLogout} />
+        <Intro />
         <br />
         <div className='p-5 text-center bg-light'>
             <h4 id="about">About Us</h4>
