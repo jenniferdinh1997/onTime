42fb2a66b813cbbed95e1eb95f9ebe1259b0439b
jenniferdinh1997 <jenniferdinh1997@gmail.com>
2022-06-08 00:18:30 -0700

    add about

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ export default function Home({ user, handleLogout }) {
         <div className='whole-body'>
             <Header user={user} handleLogout={handleLogout} />
             <Intro />
-            <div className='p-5 text-center bg-light'>
+            {/* <div className='about-section'>
                 <h4 id="about">About Us</h4>
                 <p>Our founder had previously worked as a medical assistant at a clinic and noticed that many of
                 the patients would either be late or cancel their appointments due to missing the bus, not having
@@ -24,6 +24,21 @@ export default function Home({ user, handleLogout }) {
                 </p>
                 <br />
                 <p>Drivers are questioned and interviewed thoroughly to ensure quality and compassionate care to passengers.</p>
+            </div> */}
+            <h4>Let us take care of your transportation so you can take care of yourself</h4>
+            <div className='description'>
+                <div className='desc-left'>
+                    <img src={'/home1.png'} className='home1' />
+                    <h6>Quality Care </h6>
+                    <p>Drivers are interviewed thoroughly and required CPR certification to ensure quality and compassionate care to passengers.
+                    Your safety and well-being is our highest priority. </p>
+                </div>
+
+                <div className='desc-right'>
+                    <img src={'/home2.png'} className='home2' />
+                    <h6>Patient-Driver Compatibility</h6>
+                    <p>Patients are matched specifically to a driver that speaks the same language and has a WAV if needed.</p>
+                </div>
             </div>
         </div>
     )
